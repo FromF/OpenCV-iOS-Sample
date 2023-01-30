c3f3f63dcfb412495178b3dd023b7e319f8e840f
Haruhito Fuji <haruhito.fuji@gmail.com>
2023-01-31 07:44:06 +0900

    新規プロジェクト追加

---
+++ OpenCVSample/OpenCVSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCVSampleApp.swift
+//  OpenCVSample
+//
+//  Created by 藤治仁 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct OpenCVSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
